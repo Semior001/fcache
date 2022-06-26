a932aadb447b54d619022b205d503ac9002700a6
Yelshat Duskaliyev <ura2178@gmail.com>
2022-06-26 14:39:42 +0300

    feat: added option functions

--- option.go
+++ option.go
@@ -13,3 +13,21 @@ type Options struct {
 
 // Option is a function to apply options.
 type Option func(*Options)
+
+// WithTTL sets the TTL duration for cache items.
+func WithTTL(ttl time.Duration) Option {
+	return func(o *Options) { o.TTL = ttl }
+}
+
+// WithLogger sets logger for cache.
+// `log` package is used by default.
+func WithLogger(log Logger) Option {
+	return func(o *Options) { o.Log = log }
+}
+
+// WithInvalidationPeriod sets the period between cache's checks for expired
+// items. Useful for caches, like S3.
+// No manual invalidation by default.
+func WithInvalidationPeriod(period time.Duration) Option {
+	return func(o *Options) { o.InvalidatePeriod = period }
+}
